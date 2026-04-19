317059b68300aff2c947e98397b3df318f29ae98
Apple <apple@Apples-MacBook-Pro.local>
2026-04-19 15:37:04 +0530

    udoated session create logic

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -16,11 +16,13 @@ class Users::SessionsController < Devise::SessionsController
 
   # POST /resource/sign_in
   def create
-    self.resource = warden.authenticate!(auth_options)
-    set_flash_message!(:notice, :signed_in)
-    sign_in(resource_name, resource)
-    yield resource if block_given?
-    respond_with resource, location: after_sign_in_path_for(resource)
+    self.resource = warden.authenticate(auth_options)
+    if resource
+      sign_in(resource_name, resource)
+      render json: { message: "Signed in successfully.", user: { id: resource.id, email: resource.email } }, status: :ok
+    else
+      render json: { errors: ["Invalid email or password."] }, status: :unauthorized
+    end
   end
 
   # DELETE /resource/sign_out
