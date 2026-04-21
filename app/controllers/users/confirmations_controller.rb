ffe47ffe2ddce6f696f9ec12d043e38def7b50a8
Apple <apple@Apples-MacBook-Pro.local>
2026-04-21 23:53:11 +0530

    fix unsafe redirect by passing allow_other_host to FRONTEND_URL redirect

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 class Users::ConfirmationsController < Devise::ConfirmationsController
-  protected
+  def show
+    self.resource = resource_class.confirm_by_token(params[:confirmation_token])
 
-  def after_confirmation_path_for(resource_name, resource)
-    ENV.fetch("FRONTEND_URL", "http://localhost:3000")
+    if resource.errors.empty?
+      redirect_to ENV.fetch("FRONTEND_URL", "http://localhost:3000"), allow_other_host: true
+    else
+      render json: { errors: resource.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 end
