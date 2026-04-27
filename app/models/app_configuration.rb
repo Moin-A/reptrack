7428b07c8e20da8b50c4421213e7f37db2aebc93
Apple <apple@Apples-MacBook-Pro.local>
2026-04-28 03:33:46 +0530

    replace Task.all with hardcoded task list and add task_buckets config preference

--- app/models/app_configuration.rb
+++ app/models/app_configuration.rb
@@ -1,12 +1,6 @@
 class AppConfiguration < Reptrack::Configuration
-    preference :slot_credit_packages, :array, default: [
-      {
-        key: "starter",
-        name: "Starter",
-        sessions: 1,
-        amount: 1,
-        popular: false,
-        theme: "emerald",
-        description: "Book a single session now.<br>You can always buy more later!"
-      }]
-end
+    preference :task_buckets, :array, default: [
+     "today" ,
+     "tomorrow"
+  ]
+end 
