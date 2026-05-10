ff91d852719bf26d80a745a25f41636564da01a5
Apple <apple@Apples-MacBook-Pro.local>
2026-05-10 18:21:17 +0530

    fixes

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -9,7 +9,7 @@ class ActivitiesController < ApplicationController
   def filtered_users
     scope = Audit::Version.all
     scope = scope.performed_by(activities_params[:by]) if activities_params[:by].present?
-    scope = scope.performed_on_or_before(*resolved_date_time) if activities_params[:when].present?
+    scope = scope.performed_on_or_before(*resolved_date_time) if resolved_date_time.present?
     scope
   end
 
--- app/models/Audit/version.rb
+++ app/models/Audit/version.rb
@@ -6,15 +6,11 @@ module Audit
     scope :performed_on_or_before, ->(start_date, end_date) {where("created_at > ? AND created_at < ?", start_date, end_date)}
 
     def self.resolve_date_string(date_string)
-      case date_string
-      when "today"        
-        [Time.current.beginning_of_day, Time.current.end_of_day]
-      when "past_2_days"  
-        [2.days.ago, Time.current]
-      when "past_week"    
-        [1.week.ago, Time.current]
-      when "past_30_days" 
-        [30.days.ago, Time.current]
+      case date_string.to_s.strip.gsub(" ", "_").downcase
+      when "today"        then [Time.current.beginning_of_day, Time.current.end_of_day]
+      when "past_2_days"  then [2.days.ago, Time.current]
+      when "past_week"    then [1.week.ago, Time.current]
+      when "past_30_days" then [30.days.ago, Time.current]
       end
     end
   end
