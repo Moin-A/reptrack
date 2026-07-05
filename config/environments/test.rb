6470025bbaf21e8415c2089830ca263dddf6ec49
Moin Ahmed <moinahmedptw@gmail.com>
2026-07-05 13:43:31 +0530

    added the env config in test.rb to make the test pass

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -64,4 +64,8 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
+
+  config.active_record.encryption.primary_key         = "test_primary_key_test_primary_key"
+  config.active_record.encryption.deterministic_key   = "test_deterministic_key_determin"
+  config.active_record.encryption.key_derivation_salt = "test_key_derivation_salt_salt_00"
 end
